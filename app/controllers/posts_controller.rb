d1d13c805d33b83041aaf12726554bf3f6be8955
Lisa Lee <snickers495@gmail.com>
2017-12-02 18:54:06 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,15 +21,15 @@ class PostsController < ApplicationController
 	  @post.save
 	  redirect_to post_path(@post)
 	end
-	 
+
 	def update
 	  @post = Post.find(params[:id])
 	  @post.update(post_params)
 	  redirect_to post_path(@post)
 	end
-	 
+
 	private
-	 
+
 	def post_params
 	  params.require(:post).permit(:title, :description)
 	end
